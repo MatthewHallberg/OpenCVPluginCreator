222cf80f03e6ff7ee4ef3a5c625f391b9caedd93
Matthew Hallberg <matthewhallberg@gmail.com>
2019-07-21 06:09:15 -0400

    added fix for larger resolution.

--- ObjectDection/main.cpp
+++ ObjectDection/main.cpp
@@ -10,7 +10,7 @@ Mat final_output;
 
 void mouseEvent(int evt, int x, int y, int flags, void* param) {
     if (evt == EVENT_LBUTTONDOWN) {
-        background = frame;
+        frame.copyTo(background);
         cout << "Background Changed!" << endl;
     }
 }
@@ -24,13 +24,12 @@ int main(int argc, const char * argv[]) {
     VideoCapture capture(0);
     while(!capture.isOpened()) {
         cout << "Camera" << endl;
-        usleep(1000000);
     }
     
     while (true) {
         capture.read(frame);
         resize(frame, frame, Size(frame.cols/2, frame.rows/2));
-        
+
         // Laterally invert the image / flip the image
         flip(frame,frame,1);
         
@@ -39,37 +38,39 @@ int main(int argc, const char * argv[]) {
             //Converting image from BGR to HSV color space.
             Mat hsv;
             cvtColor(frame, hsv, COLOR_BGR2HSV);
-            
+
             Mat mask1,mask2;
             // Creating masks to detect the upper and lower bounds of color
-            
+
             //red
             //            inRange(hsv, Scalar(0, 180, 70), Scalar(10, 255, 255), mask1);
             //            inRange(hsv, Scalar(170, 120, 70), Scalar(180, 255, 255), mask2);
-            
+
             //green
             inRange(hsv, Scalar(35, 40, 20), Scalar(100, 255, 255), mask1);
             inRange(hsv, Scalar(290, 100, 70), Scalar(300, 100, 100), mask2);
-            
+
             // Generating the final mask
             mask1 = mask1 + mask2;
-            
+
             Mat kernel = Mat::ones(3,3, CV_32F);
             morphologyEx(mask1,mask1,cv::MORPH_OPEN,kernel);
             morphologyEx(mask1,mask1,cv::MORPH_DILATE,kernel);
-            
+
             // creating an inverted mask to segment out the cloth from the frame
             bitwise_not(mask1,mask2);
             Mat res1, res2, final_output;
-            
+
             // Segmenting the cloth out of the frame using bitwise and with the inverted mask
             bitwise_and(frame,frame,res1,mask2);
-            
+
             // creating image showing static background frame pixels only for the masked region
             bitwise_and(background,background,res2,mask1);
-            
+
             // Generating the final augmented output.
             addWeighted(res1,1,res2,1,0,final_output);
+            
+            cout << "WTF" << endl;
             
             imshow("camWindow", final_output);
             
